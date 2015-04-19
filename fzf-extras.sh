c760f5ba1bd04861edc4342dad22c66bd612cfc2
Antoni Kaniowski <a@generativestuff.com>
2015-04-19 21:37:23 +0200

    added checkout functionality to fshow

--- fzf-extras.sh
+++ fzf-extras.sh
@@ -87,13 +87,15 @@ fshow() {
       git log --graph --color=always \
           --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
       fzf --ansi --multi --no-sort --reverse --query="$q" \
-          --print-query --expect=ctrl-d --toggle-sort=\`); do
+          --print-query --expect=ctrl-o,ctrl-d --toggle-sort=\`); do
     q=$(head -1 <<< "$out")
     k=$(head -2 <<< "$out" | tail -1)
     shas=$(sed '1,2d;s/^[^a-z0-9]*//;/^$/d' <<< "$out" | awk '{print $1}')
     [ -z "$shas" ] && continue
     if [ "$k" = 'ctrl-d' ]; then
       git diff --color=always $shas | less -R
+    elif [ "$k" = 'ctrl-o' ]; then
+      git checkout $shas 
     else
       for sha in $shas; do
         git show --color=always $sha | less -R
