73314b638d31b596603353b908149f1850e9ee71
Andy Weidenbaum <archbaum@gmail.com>
2015-07-03 12:45:12 -0700

    simplified fshow, taken from junegunn/dotfiles
    - <kbd>Enter</kbd> to diff commits
    
    https://github.com/junegunn/dotfiles/commit/aa9d0c382167acc6e086236ac906a2cd6a4c5b81

--- fzf-extras.sh
+++ fzf-extras.sh
@@ -95,28 +95,14 @@ fcoc() {
   git checkout $(echo "$commit" | sed "s/ .*//")
 }
 
-# fshow - git commit browser (enter for show, ctrl-d for diff, backtick ` toggles sort)
+# fshow - git commit browser
 fshow() {
-  local out shas sha q k
-  while out=$(
-      git log --graph --color=always \
-          --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-      fzf --ansi --multi --no-sort --reverse --query="$q" \
-          --print-query --expect=ctrl-o,ctrl-d --toggle-sort=\`); do
-    q=$(head -1 <<< "$out")
-    k=$(head -2 <<< "$out" | tail -1)
-    shas=$(sed '1,2d;s/^[^a-z0-9]*//;/^$/d' <<< "$out" | awk '{print $1}')
-    [ -z "$shas" ] && continue
-    if [ "$k" = 'ctrl-d' ]; then
-      git diff --color=always $shas | less -R
-    elif [ "$k" = 'ctrl-o' ]; then
-      git checkout $shas
-    else
-      for sha in $shas; do
-        git show --color=always $sha | less -R
-      done
-    fi
-  done
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --toggle-sort=\` \
+      --bind "ctrl-m:execute:
+                echo '{}' | grep -o '[a-f0-9]\{7\}' |
+                xargs -I % sh -c 'git show --color=always % | less -R'"
 }
 
 # fcs - get git commit sha
