5187f6106b9ad3a7420dffb994385c7b6f7c8c2b
Andy Weidenbaum <archbaum@gmail.com>
2015-07-15 16:56:21 -0700

    fshow - line can contain multiple commit-hash like substrings
    https://github.com/junegunn/dotfiles/commit/c4b53bbc1749217ba42ba46a419ffd462aca9368

--- fzf-extras.sh
+++ fzf-extras.sh
@@ -101,7 +101,7 @@ fshow() {
       --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
   fzf --ansi --no-sort --reverse --tiebreak=index --toggle-sort=\` \
       --bind "ctrl-m:execute:
-                echo '{}' | grep -o '[a-f0-9]\{7\}' |
+                echo '{}' | grep -o '[a-f0-9]\{7\}' | head -1 |
                 xargs -I % sh -c 'git show --color=always % | less -R'"
 }
 
