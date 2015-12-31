54a07a7af29a0fbe040ec023eb41c7b3fd94f971
Andy Weidenbaum <archbaum@gmail.com>
2015-12-30 20:09:26 -0800

    fixed fshow escape, from junegunn/dotfiles
    https://github.com/junegunn/dotfiles/commit/4ead47d6e6af9d7398c0c0340419c5720167bb29

--- fzf-extras.sh
+++ fzf-extras.sh
@@ -116,10 +116,11 @@ fcoc() {
 fshow() {
   git log --graph --color=always \
       --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-  fzf --ansi --no-sort --reverse --tiebreak=index --toggle-sort=\` \
+  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
       --bind "ctrl-m:execute:
-                echo '{}' | grep -o '[a-f0-9]\{7\}' | head -1 |
-                xargs -I % sh -c 'git show --color=always % | less -R'"
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}"
 }
 
 # fcs - get git commit sha
