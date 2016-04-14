50dccca65068dfcd79d7051e37303e097b1a0cdc
Andy Weidenbaum <archbaum@gmail.com>
2016-04-14 08:08:59 -0700

    fco -1, from junegunn/dotfiles
    https://github.com/junegunn/dotfiles/commit/f3d189aca0cc1ba3c4859fdea394096c3552725b

--- fzf-extras.sh
+++ fzf-extras.sh
@@ -100,7 +100,7 @@ fco() {
     sort -u          | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}') || return
   target=$(
     (echo "$tags"; echo "$branches") |
-    fzf-tmux -l30 -- --no-hscroll --ansi +m -d "\t" -n 2) || return
+    fzf-tmux -l30 -- --no-hscroll --ansi +m -d "\t" -n 2 -1 -q "$*") || return
   git checkout $(echo "$target" | awk '{print $2}')
 }
 
