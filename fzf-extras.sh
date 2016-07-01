e49f77b7ef9096fa6374463dd479b3133f1f68cb
Andy Weidenbaum <archbaum@gmail.com>
2016-07-01 09:20:37 -0700

    update fshow, from junegunn dotfiles
    note: now requires vim-fugitive → produces colored diff output
    
    https://github.com/junegunn/dotfiles/commit/e32a662bd1ea2ff4be9e51ea8323629e5d56caac

--- fzf-extras.sh
+++ fzf-extras.sh
@@ -121,11 +121,11 @@ fshow() {
   git log --graph --color=always \
       --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
   fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
-      --bind "ctrl-m:execute:
-                (grep -o '[a-f0-9]\{7\}' | head -1 |
-                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
-                {}
-FZF-EOF"
+      --header "Press CTRL-S to toggle sort" \
+      --preview "echo {} | grep -o '[a-f0-9]\{7\}' | head -1 |
+                 xargs -I % sh -c 'git show --color=always % | head -$LINES '" \
+      --bind "enter:execute:echo {} | grep -o '[a-f0-9]\{7\}' | head -1 |
+              xargs -I % sh -c 'vim fugitive://\$(git rev-parse --show-toplevel)/.git//% < /dev/tty'"
 }
 
 # fcs - get git commit sha
