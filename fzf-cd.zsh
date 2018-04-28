710144a8ba57e02badc3a9aca9d35575702b7117
u1and0 <e01.ando60@gmail.com>
2018-04-28 23:55:48 +0900

    [mod]bindkey modify to Alt+g@fzf-cd.zsh

--- fzf-cd.zsh
+++ fzf-cd.zsh
@@ -1,14 +1,14 @@
 #!/bin/zsh
-# Alt+dでfileのあいまい検索を行い、そのファイルがあるディレクトリにcd
+# fileのあいまい検索を行い、そのファイルがあるディレクトリにcd
 # fzf-cd-widget(Alt+c)ではディレクトリ検索しかできなかった点を改造
 
-# ALT-D - cd into the directory contained selected file
+# ALT-G - cd into the directory contained selected file
 fzf-cd-basename-widget() {
-    dirname $(fd --type f | fzf) | cd
+    dirname $(fzf) | cd
     local ret=$?
     zle fzf-redraw-prompt
     typeset -f zle-line-init >/dev/null && zle zle-line-init
     return $ret
 }
 zle -N fzf-cd-basename-widget
-bindkey '\ed' fzf-cd-basename-widget
+bindkey '\eg' fzf-cd-basename-widget
