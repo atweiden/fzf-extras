4a9c576cbb70ea6a3f8c040c34e906280d04ce6a
u1and0 <e01.ando60@gmail.com>
2018-05-06 11:43:31 +0900

    [mod]height 100% @fzf-gitlog-widget & fzf-gitlog-multi-widget

--- fzf-extras.sh
+++ fzf-extras.sh
@@ -229,7 +229,7 @@ fzf-gitlog-widget() {
     --format=format:'%C(yellow)%h %C(reset)%s %C(bold black)%cd %C(auto)%d %C(reset)'\
     --date=short --color=always"
 
-    fzf_cmd="fzf --ansi --reverse --no-sort --tiebreak=index\
+    fzf_cmd="fzf --height 100% --ansi --reverse --no-sort --tiebreak=index\
     --bind=ctrl-x:toggle-sort\
     --bind \"ctrl-m:execute: (grep -o '[a-f0-9]\{7\}' | head -1 |
         xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
@@ -257,7 +257,7 @@ fzf-gitlog-multi-widget() {
     --format=format:'%C(yellow)%h %C(reset)%s %C(bold black)%cd %C(auto)%d %C(reset)'\
     --date=short --color=always"
 
-    fzf_cmd="fzf --multi --ansi --reverse --no-sort --tiebreak=index\
+    fzf_cmd="fzf --height 100% --multi --ansi --reverse --no-sort --tiebreak=index\
     --bind=ctrl-x:toggle-sort"
 
     eval "$git_cmd | $fzf_cmd" |
